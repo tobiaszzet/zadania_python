8de60b4f53fb74bbb285a9c4d142f58ed888572a
Tobiasz Zajusz <t.zajusz@gmail.com>
2024-04-10 14:26:49 +0200

    dodano sprawdzenie, czy podano co najmniej 2 argumenty sys. oraz czy pliki input/output istnieją

--- manager_csv.py
+++ manager_csv.py
@@ -1,13 +1,23 @@
 import sys
-import csv
+import os
 from file_handler import FileHandler
 
 
+
 def load_sys_args():
-    return sys.argv[1], sys.argv[2], sys.argv[3:]
+    if len(sys.argv) >= 3:
+        return sys.argv[1], sys.argv[2], sys.argv[3:]
+    else:
+        print("Error. Wymagane są 3 arguementy systemowe")
 
 
 input_file, output_file, changes = load_sys_args()
+
+if not os.path.exists(input_file):
+    print("Plik wejściowy nie istnieje.")
+if not os.path.exists(output_file):
+    print("Plik wyjściowy nie istnieje. Zmiany nie zostały zapisane do pliku")
+
 
 file_handler = FileHandler(input_file, output_file, changes)
 print(f"input: {file_handler.manager}")
