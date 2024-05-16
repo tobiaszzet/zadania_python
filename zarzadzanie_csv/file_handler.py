39db06c33804aa753dd5164cbe6f7a28e217cc9e
tobiaszzet <129624955+tobiaszzet@users.noreply.github.com>
2024-05-17 01:56:14 +0200

    optymalizacja pod formaty .json .txt .pkl
    przeprojektowanie kodu w oparciu o dziedziczenie oraz optymalizacja  pod formaty plików .json .txt .pkl

--- zarzadzanie_csv/file_handler.py
+++ zarzadzanie_csv/file_handler.py
@@ -1,14 +1,35 @@
+from abc import ABC, abstractmethod
+import pickle
 import csv
+import json
 
 
-class FileHandler:
+class FileHandler(ABC):
     def __init__(self, input_file, output_file, changes):
         self.input_file = input_file
         self.output_file = output_file
         self.changes = changes
-        self.manager = self.load_data_from_csv()
+        self.data = None
 
-    def load_data_from_csv(self):
+    @abstractmethod
+    def read_input_file(self):
+        pass
+
+    @abstractmethod
+    def save_to_file(self):
+        pass
+
+    def transform(self):
+        for transformation in self.changes:
+            transformation_list = transformation.split(",")
+            column = int(transformation_list[0])
+            row = int(transformation_list[1])
+            change = transformation_list[2]
+            self.data[row][column] = change
+
+
+class CSVFileHandler(FileHandler):
+    def read_input_file(self):
         temporary = []
         with open(self.input_file) as file:
             data = csv.reader(file)
@@ -17,18 +38,55 @@ class FileHandler:
                 for value in line:
                     temp_line.append(value)
                 temporary.append(temp_line)
-        return temporary
+        print(f"temporary {temporary}")
+        self.data = temporary
 
-    def save_data_to_csv(self):
+    def save_to_file(self):
         with open(self.output_file, mode="w") as file:
             writer = csv.writer(file)
-            for line in self.manager:
+            for line in self.data:
                 writer.writerow(line)
 
-    def transform(self):
-        for transformation in self.changes:
-            transformation_list = transformation.split(",")
-            column = int(transformation_list[0])
-            row = int(transformation_list[1])
-            change = transformation_list[2]
-            self.manager[row][column] = change
+
+class PickleFileHandler(FileHandler):
+    def read_input_file(self):
+        with open(self.input_file, mode="rb") as file:
+            self.data = pickle.load(file)
+
+    def save_to_file(self):
+        with open(self.output_file, mode="wb") as file:
+            pickle.dump(self.data, file)
+
+
+class JsonFileHandler(FileHandler):
+    def read_input_file(self):
+        with open(self.input_file) as file:
+            data = json.load(file)
+            temporary = []
+            for key, values in data.items():
+                temporary.append(values)
+            self.data = temporary
+
+    def save_to_file(self):
+        with open(self.output_file, mode="w") as file:
+            temp = {}
+            for key, value in enumerate(self.data):
+                temp[key] = value
+            json.dump(temp, file)
+
+
+class TXTFileHandler(FileHandler):
+    def read_input_file(self):
+        temporary = []
+        with open(self.input_file) as file:
+            data = csv.reader(file)
+            for line in data:
+                temp_line = []
+                for value in line:
+                    temp_line.append(value)
+                temporary.append(temp_line)
+        self.data = temporary
+
+    def save_to_file(self):
+        with open(self.output_file, mode="w") as file:
+            file.write(str(self.data))
--- zarzadzanie_csv/manager_csv.py
+++ zarzadzanie_csv/manager_csv.py
@@ -1,16 +1,51 @@
 import sys
-import csv
-from file_handler import FileHandler
+import os
+from file_handler import CSVFileHandler, PickleFileHandler, JsonFileHandler, TXTFileHandler
 
 
 def load_sys_args():
-    return sys.argv[1], sys.argv[2], sys.argv[3:]
+    if len(sys.argv) >= 3:
+        return sys.argv[1], sys.argv[2], sys.argv[3:]
+    else:
+        print("Error. Wymagane są 3 arguementy systemowe")
 
 
 input_file, output_file, changes = load_sys_args()
 
-file_handler = FileHandler(input_file, output_file, changes)
-print(f"input: {file_handler.manager}")
-file_handler.transform()
-print(f"output: {file_handler.manager}")
-file_handler.save_data_to_csv()
+if not os.path.exists(input_file):
+    print("Plik wejściowy nie istnieje.")
+if not os.path.exists(output_file):
+    print("Plik wyjściowy nie istnieje. Zmiany nie zostały zapisane do pliku")
+
+input_file_handler = None
+output_file_handler = None
+
+if input_file.endswith(".txt"):
+    input_file_handler = TXTFileHandler(input_file, output_file, changes)
+elif input_file.endswith(".pkl"):
+    input_file_handler = PickleFileHandler(input_file, output_file, changes)
+elif input_file.endswith("json"):
+    input_file_handler = JsonFileHandler(input_file, output_file, changes)
+elif input_file.endswith("csv"):
+    input_file_handler = CSVFileHandler(input_file, output_file, changes)
+else:
+    print("nie mamy takiego pliku")
+
+
+if output_file.endswith(".txt"):
+    output_file_handler = TXTFileHandler(input_file, output_file, changes)
+elif output_file.endswith(".pkl"):
+    output_file_handler = PickleFileHandler(input_file, output_file, changes)
+elif output_file.endswith("json"):
+    output_file_handler = JsonFileHandler(input_file, output_file, changes)
+elif output_file.endswith("csv"):
+    output_file_handler = CSVFileHandler(input_file, output_file, changes)
+else:
+    print("nie mamy takiego pliku")
+
+input_file_handler.read_input_file()
+print(f"input: {input_file_handler.data}")
+input_file_handler.transform()
+output_file_handler.data = input_file_handler.data
+print(f"output: {output_file_handler.data}")
+output_file_handler.save_to_file()
